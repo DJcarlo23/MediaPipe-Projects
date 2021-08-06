0b7fd50fd31c26c2d2710abb2914c80d2b7e92de
DJcarlo <krystian.wicherek89@gmail.com>
2021-08-06 16:00:27 +0200

    I made some changes with comments

--- FaceDetection/face_detection.py
+++ FaceDetection/face_detection.py
@@ -15,14 +15,9 @@ with mp_face_detection.FaceDetection(
     success, image = cap.read()
     if not success:
       print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
       continue
 
-    # Flip the image horizontally for a later selfie-view display, and convert
-    # the BGR image to RGB.
     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
     image.flags.writeable = False
     results = face_detection.process(image)
 
